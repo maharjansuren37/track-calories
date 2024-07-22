c53f1879b9175364bd6d6c0a6f151ed8a2abc38c
maharjansuren37 <maharjansuren37@gmail.com>
2024-07-22 20:36:42 +0930

    initial commit

---
+++ TrackCalories/Models/Calories.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrackCalories.Models
+{
+    public class Calories
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description {  get; set; }
+
+    }
+}
